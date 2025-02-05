0817932ff1f4b794be87566b92a8f02d3d35b5e6
Samara Martínez <s.martinezj@uniandes.edu.co>
2025-02-05 10:27:33 -0500

    Esqueleto Operario y Deposito

---
+++ Deposito.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Deposito {
+    public ArrayList productos = new ArrayList();
+    public int totalnecesario;
+
+    public void agregarProducto(){
+
+    }
+
+    public void haAlcanzadoMeta(){
+        
+    }
+
+
+
+    
+}
---
+++ OperarioCalidad.java
@@ -0,0 +1,18 @@
+public class OperarioCalidad {
+    public int id;
+    public BuzonRevision buzonRevision;
+    public BuzonReproceso buzonReproceso;
+    public Deposito deposito;
+    public int productosAprovar;
+    public int maxRechazos;
+
+    public void run(){
+
+    }
+
+    public void revisarProdicto(){
+
+    }
+
+    
+}
